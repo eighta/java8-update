0873f8b4dea650c1eefd95e826099472b9fc7b44
eighta <ochoalarios@gmail.com>
2017-06-04 02:59:05 -0500

    init

---
+++ java8-update/src/test/java/oca/Chapter2AgainTest.java
@@ -0,0 +1,5 @@
+package oca;
+
+public class Chapter2AgainTest {
+
+}
---
+++ java8-update/src/test/java/ocp/OcpChapter1Test.java
@@ -0,0 +1,33 @@
+package ocp;
+
+public class OcpChapter1Test {
+
+	{
+		
+		/*
+		Review Questions
+		1	
+		2
+		3
+		4
+		5
+		6
+		7
+		8
+		9
+		10
+		11
+		12
+		13
+		14
+		15
+		16
+		17
+		18
+		19
+		20
+		21
+		
+		*/
+	}
+}
