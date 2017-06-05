8a7aa13bbc082b2075661ac9f583038fb01f9d8f
eighta <ochoalarios@gmail.com>
2017-06-04 21:08:27 -0500

    ocp chapter 2 half

---
+++ src/test/java/ocp/OcpChapter3Test.java
@@ -0,0 +1,20 @@
+package ocp;
+
+public class OcpChapter3Test {
+
+	public interface CanClimb {
+		public abstract void climb();
+	}
+	
+	public static void main(String[] args) {
+		new CanClimb() {
+			
+			@Override
+			public void climb() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+	}
+	
+}
