18ec657da1ae6716805345c199da18b355b5325f
eighta <ochoalarios@gmail.com>
2018-03-30 14:48:00 -0500

    jdbc

---
+++ src/main/java/core/JavaJDBC.java
@@ -0,0 +1,51 @@
+package core;
+
+public class JavaJDBC {
+
+	{
+/**
+JDBC stands for Java Database Connectivity. 
+
+Introducing Relational Databases
+and SQL
+Data is information. A piece of data is one fact, such as your first name. A database is an
+organized collection of data. In the real world, a file cabinet is a type of database. It has file
+folders, each of which contains pieces of paper. The file folders are organized in some way,
+often alphabetically. Each piece of paper is like a piece of data. Similarly, the folders on your
+computer are like a database. The folders provide organization, and each file is a piece of data.
+A relational database is a database that is organized into tables, which consist of rows
+and columns. You can think of a table as a spreadsheet. There are two main ways to access
+a relational database from Java:
+■■ Java Database Connectivity Language (JDBC): Accesses data as rows and columns.
+JDBC is the API covered in this chapter.
+■■ Java Persistence API (JPA): Accesses data through Java objects using a concept called
+object-relational mapping (ORM). The idea is that you don’t have to write as much
+code, and you get your data in Java objects. JPA is not on the exam, and therefore it is
+not covered in this chapter.
+■■ A relational database is accessed through Structured Query Language (SQL).
+■■ In addition to relational databases, there is another type of database called a NoSQL
+database. This is for databases that store their data in a format other than tables.
+NoSQL is out of scope for the exam as well.
+■■ In the following sections, we introduce a small relational database that we will be
+using for the examples in this chapter and present the SQL to access it. We will also
+cover some vocabulary that you need to know.
+		
+In all of the other chapters of this book, you need to write code and try lots of examples.
+This chapter is different. It’s still nice to try out the examples, but you can probably get
+the JDBC questions correct on the exam from just reading this chapter and mastering the
+review questions.
+Luckily, Java comes with an embedded database called JavaDB. JavaDB is a
+version of the open source Derby database that comes automatically with the JDK
+(http://db.apache.org/derby). To practice for the exam, Derby is sufficient.
+		
+		
+ */
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new JavaJDBC();
+	}
+}
